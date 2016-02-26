a00b6f9f450429a91960c5be278e6e637fa5843b
chen <chen@localhost.localdomain>
2016-02-26 10:14:56 +0800

    methods in views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def list_books(request):
+	return HttpResponse('we could put anything here')
