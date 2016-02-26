3c2daa87d75eae41ed7e2d04fd7d0db5b7e9c55b
chen <chen@localhost.localdomain>
2016-02-26 13:24:27 +0800

    add cbv

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Book
-
+from django.views.generic import DetailView,View
+from .models import Book,Author
+from django.db.models import Count
 
 # Create your views here.
 def list_books(request):
@@ -15,3 +16,27 @@ def list_books(request):
 
 
 	return render(request,"list.html",context)
+
+
+
+class AuthorList(View):
+	def get(self,request):
+		authors = Author.objects.annotate(published_books=Count('books')
+			).filter(
+				published_books__gt=0
+			)
+
+		context={
+			'authors':authors,
+		}
+		return render(request,'authors.html',context)
+
+
+class BookDetail(DetailView):
+	model =Book
+	template_name = 'book.html'
+
+
+class AuthorDetail(DetailView):
+	model = Author
+	template_name = 'author.html'
