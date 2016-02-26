3d27642418e942d1ab7583b61da0c1151b3e4380
chen <chen@localhost.localdomain>
2016-02-26 12:45:57 +0800

    create list_books view and template

--- books/models.py
+++ books/models.py
@@ -15,11 +15,15 @@ class Book(models.Model):
 	def list_authors(self):
 		return ", ".join([author.name for author in self.authors.all()])
 
-	def save(self,*args,**kwargs):
-		if (self.review and self.date_received is None):
-			self.date_received = now()
+		
+	def save(self, *args, **kwargs):
+		if (self.review and self.date_reviewed is None):
+			self.date_reviewed = now()
+			
+		super(Book, self).save(*args, **kwargs)
 
-		super(Book,self).save(*args,**kwargs)
+
+
 
 class Author(models.Model):
 	name = models.CharField(max_length=70,help_text='use pen name, not real name',unique=True)
